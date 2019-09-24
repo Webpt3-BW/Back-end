8c6a4ec2750ba371e0f35cf91a45d6fe5c23dfd5
cocoitali <cmburatto@gmail.com>
2019-09-24 12:15:42 -0600

    Updated dependencies/requirements.txt for allauth, migrated to db

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from django.conf.urls import url
 
 urlpatterns = [
-    path('registration/', include('rest_auth.urls'))
+    path('registration/', include('rest_auth.urls')),
+    path('registration/', include('rest_auth.registration.urls'))
 ]
