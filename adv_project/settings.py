a17b5c02044ed9dc9892e7ee1c4a30ceb2d1b0ae
cocoitali <cmburatto@gmail.com>
2019-09-24 11:28:22 -0600

    Fixed bug in settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adventure'
+    'adventure',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
